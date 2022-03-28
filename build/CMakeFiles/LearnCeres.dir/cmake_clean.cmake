741dc9aa3d3811c9161eb88bdf597eea5912aa26
hongfeng <1256568522@qq.com>
2022-03-28 16:10:59 +0800

    first commit

---
+++ build/CMakeFiles/LearnCeres.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LearnCeres.dir/cereslearning1.cpp.o"
+  "CMakeFiles/LearnCeres.dir/cereslearning1.cpp.o.d"
+  "LearnCeres"
+  "LearnCeres.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LearnCeres.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
